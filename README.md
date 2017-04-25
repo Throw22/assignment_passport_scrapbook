99a7b069237c2a4fc2e7019b7c2e952924d0f36b
markmarkyesyes <markhahn545@gmail.com>
2017-04-25 14:41:56 -0500

    deleted files

--- README.md
+++
@@ -1,3 +0,0 @@
-# assignment_passport_scrapbook
-
-Make an amalagam of user information gathered from a gallimaufry of OAuth providers.
--- erase.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const User = require("./models/user");
-mongoose.connect("mongodb://localhost/passport-demo");
-
-User.remove({}, function(err) {
-  console.log("collection removed");
-});
